4ff796aba66c20860ee268222dabf74c3435f939
Rhianna So <38052944+rhiannaso@users.noreply.github.com>
2020-08-06 23:49:31 -0700

    Second draft of contact page

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -4,16 +4,34 @@ import ResumeImg from '../Images/resume.jpg';
 import DL from '../Images/download.png';
 import Mail from '../Images/mail.png';
 import LinkedIn from '../Images/linked_in.png';
-import GH from '../Images/gh2.png';
+import GH from '../Images/gh.png';
+import Open from '../Images/open.png';
 
 export class Contact extends Component {
+  constructor()
+  {
+    super();
+    this.state = {
+      display: 'none',
+      filter: 'brightness(1.0)'
+    }
+  }
+  showMsg = () => {
+    this.setState({ display: 'flex', filter: 'brightness(0.5)' });
+  }
+  hideMsg = () => {
+    this.setState({ display: 'none', filter: 'brightness(1.0)' });
+  }
   render() {
     return(
       <div id="contact">
         <div className="contact-wrap">
           <div className="lh-side">
             <a href={Resume} target="_blank">
-              <img src={ResumeImg} className="resume" />
+              <img src={ResumeImg} className="resume" onMouseOver={this.showMsg} onMouseOut={this.hideMsg} style={{filter: this.state.filter, transition: 'ease-in .1s'}} />
+              <div className="open-msg" onMouseOver={this.showMsg} onMouseOut={this.hideMsg} style={{display: this.state.display, transition: 'ease-in .1s'}}>
+                <img src={Open} style={{width: '7vh'}} />
+              </div>
             </a>
           </div>
           <div className="rh-side">
@@ -22,10 +40,10 @@ export class Contact extends Component {
             <div className="icon-bar">
               <a href="mailto:so.rhianna@gmail.com"><img src={Mail} className="icon" /></a>
               <a href="https://www.linkedin.com/in/rhianna-so/" target="_blank"><img src={LinkedIn} className="icon" /></a>
-              <a href="https://github.com/rhiannaso" target="_blank"><img src={GH} className="icon" /></a>
+              <a href="https://github.com/rhiannaso" target="_blank"><img src={GH} className="icon-small" /></a>
             </div>
-            <a href={Resume} target="_blank" style={{color: '#FFF'}}>
-              <div className="dl-btn">Download <img src={DL} style={{marginLeft: '8px', height: '16px'}} /></div>
+            <a href={Resume} target="_blank" style={{color: "#FFF"}}>
+              <div className="dl-btn">Download <img src={DL} style={{marginLeft: "8px", height: "16px"}} /></div>
             </a>
           </div>
         </div>
